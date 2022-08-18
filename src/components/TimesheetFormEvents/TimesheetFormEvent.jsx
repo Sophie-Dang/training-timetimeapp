5782eeed09e1c5694d3cd1c195ba6e495b3ae521
Sophie-Dang <sophie.dang.kieuduyen@gmail.com>
2022-08-18 10:27:55 +0200

    feat: add TimesheetFormEvent component (empty)

---
+++ src/components/TimesheetFormEvents/TimesheetFormEvent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './TimesheetFormEvent.module.scss';
+
+function TimesheetFormEvent() {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default TimesheetFormEvent;

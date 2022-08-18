7110642ea5335259e5bb186e49e0e22cc2e36bb9
Sophie-Dang <sophie.dang.kieuduyen@gmail.com>
2022-08-18 10:45:17 +0200

    feat: add fictive datas + html base for TimesheetFormEvent component

--- src/components/TimesheetFormEvents/TimesheetFormEvent.jsx
+++ src/components/TimesheetFormEvents/TimesheetFormEvent.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import styles from './TimesheetFormEvent.module.scss';
+import React from "react";
+import styles from "./TimesheetFormEvent.module.scss";
 
 function TimesheetFormEvent() {
   return (
     <section>
-
+      <div>
+        <p>Google Agenda</p>
+        <ul>
+          <li>
+            <p>Weekly CosaVostra</p>
+            <time>Aujourd"hui de 10h15 à 11h00</time>
+            <div className="u-flex">
+              <button type="button" className="button">Timesheeter</button>
+              <button type="button" className="button">Masquer</button>
+            </div>
+          </li>
+          <li>
+            <p>Weekly CosaVostra 2</p>
+            <time>Aujourd"hui de 10h15 à 11h00</time>
+            <div className="u-flex">
+              <button type="button" className="button">Timesheeter</button>
+              <button type="button" className="button">Masquer</button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
---
+++ src/store/datas/dataEventList.js
@@ -0,0 +1,49 @@
+export const eventListGoogle = [
+  {
+    id: 0,
+    title: 'Weekly CosaVostra',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+  },
+  {
+    id: 2,
+    title: 'Evènement 2',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+  },
+  {
+    id: 3,
+    title: 'Evènement 3',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+  },
+  {
+    id: 4,
+    title: 'Evènement 4',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+  },
+];
+
+export const eventListZoho = [
+  {
+    id: 0,
+    title: 'Atland Voisin',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+    task: 'Je suis une tâche'
+  },
+  {
+    id: 2,
+    title: 'Evènement Zoho 2',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+    task: 'Je suis une tâche'
+  },
+  {
+    id: 3,
+    title: 'Evènement Zoho 3',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+    task: 'Je suis une tâche'
+  },
+  {
+    id: 4,
+    title: 'Evènement Zoho 4',
+    date: `Aujourd'hui de 10h15 à 11h00`,
+    task: 'Je suis une tâche'
+  },
+];
